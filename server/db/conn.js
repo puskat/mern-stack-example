0179c22296c5361e1fa5ae878548797940dc4cfe
puskat <hasanoyurtkulu@gmail.com>
2023-12-28 00:17:43 +0200

    health check updates

--- server/db/conn.js
+++ server/db/conn.js
@@ -6,16 +6,14 @@ const database = process.env.MONGODB_DATABASE;
 const host = process.env.MONGODB_HOST || 'mongodb';
 const port = process.env.MONGODB_PORT || 27017;
 
-
 const Db = `mongodb://${username}:${password}@${host}:${port}/${database}`;
-
 
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-let _db; // Use 'let' for potential re-assignment
+let _db;
 
 module.exports = {
   async connectToServer() {
@@ -24,22 +22,32 @@ module.exports = {
       _db = client.db("employees");
       console.log("Successfully connected to MongoDB.");
 
-      // Check if the 'employees' database exists and create it if needed
       const collections = await _db.listCollections().toArray();
       if (collections.length === 0) {
         console.log("Database 'employees' not found. Creating...");
-        await _db.createCollection("records"); // Create the 'records' collection
-        console.log(
-          "Database 'employees' and collection 'records' created successfully."
-        );
+        await _db.createCollection("records");
+        console.log("Database 'employees' and collection 'records' created successfully.");
       }
     } catch (err) {
       console.error("Error connecting to MongoDB:", err);
-      throw err; // Re-throw to propagate the error
+      throw err;
     }
   },
 
   getDb() {
     return _db;
+  },
+
+  async healthCheck() {
+    if (!_db) {
+      return false;
+    }
+    try {
+      await _db.command({ ping: 1 });
+      return true;
+    } catch (err) {
+      console.error("Database ping failed:", err);
+      return false;
+    }
   },
 };
--- server/server.js
+++ server/server.js
@@ -2,19 +2,29 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
+const dbo = require("./db/conn");
+
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
+
+app.get('/health', async (req, res) => {
+  const isHealthy = await dbo.healthCheck();
+  if (isHealthy) {
+    res.status(200).send('OK');
+  } else {
+    res.status(500).send('Database connection is unhealthy');
+  }
+});
+
 app.listen(port, async () => {
-  // perform a database connection when server starts
   try {
     await dbo.connectToServer();
     console.log(`Server is running on port: ${port}`);
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
-    process.exit(1); // Exit if connection fails
+    process.exit(1);
   }
 });
